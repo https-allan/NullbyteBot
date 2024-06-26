67645e7ab936f65388e5a1626071bab94e3553ae
https-allan <code.allanjose@gmail.com>
2024-06-25 20:21:14 -0400

    Update

---
+++ front-end/js/app.js
@@ -0,0 +1,23 @@
+async function fetchStatus() {
+  try {
+    const response = await fetch("/status");
+    const data = await response.json();
+
+    const messagesDirect = document.getElementById("messages");
+    const activityLog = document.getElementById("activityLog");
+
+    messagesDirect.innerText = data.messages.length;
+
+    data.messages.forEach((messages) => {
+      const newMessageNotification = document.createElement("li");
+
+      newMessageNotification.innerText = `Mensagem recebida de @${messages.user}: ${messages.content}`;
+
+      activityLog.appendChild(newMessageNotification);
+    });
+  } catch (error) {
+    document.getElementById("status").innerText = "Erro ao obter status";
+  }
+}
+
+fetchStatus();
---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+const MAX_MESSAGES = 11;
+
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+const discordClient = new Client({
+  intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
+  partials: [Partials.Message, Partials.Channel],
+});
+
+let receivedMessages = [];
+let discordBotStatus;
+
+discordClient.once("ready", async (client) => {
+  if (client) {
+    discordBotStatus = "Funcionando";
+  } else {
+    discordBotStatus = "Desconectado";
+  }
+});
+
+discordClient.on("messageCreate", async (message) => {
+  if (message.guild) return;
+
+  receivedMessages.push({
+    content: message.content,
+    user: message.author.tag,
+  });
+
+  if (receivedMessages.length >= MAX_MESSAGES) {
+    receivedMessages.pop();
+  }
+});
+
+discordClient.login(DISCORD_BOT_TOKEN);
+
+app.get("/status", (req, res) => {
+  res.json({
+    status: discordBotStatus,
+    messages: receivedMessages,
+  });
+});
+
+app.use(express.static("front-end"));
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
